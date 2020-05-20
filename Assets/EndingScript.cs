aef784b616ee0513623e7ed6efdebb0aeee9fb4b
maccap <60553783+maccap@users.noreply.github.com>
2020-05-20 10:56:59 +0100

    added ending just need you to fix the door in the hotel for it

---
+++ Assets/EndingScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingScript : MonoBehaviour
+{
+
+    public GameObject EndingUI;
+    public GameObject HUD;
+    public AudioSource AS;
+    public AudioClip endingSong;
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            HUD.SetActive(false);
+            EndingUI.SetActive(true);
+            AS.PlayOneShot(endingSong);    
+
+        }
+
+    }
+}
